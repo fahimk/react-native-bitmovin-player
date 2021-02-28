0118e76c902bf9bb1e0df3a1b080414bc9ad4992
Fahim Karim <fahimkarim@gmail.com>
2021-02-28 11:49:06 -0800

    more work for android

---
+++ android/src/main/java/com/takeoffmediareactnativebitmovinplayer/CustomFullscreenHandler.kt
@@ -0,0 +1,37 @@
+package com.takeoffmediareactnativebitmovinplayer
+
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import com.bitmovin.player.BitmovinPlayerView
+import com.bitmovin.player.ui.FullscreenHandler
+class CustomFullscreenHandler(
+  private val playerView: BitmovinPlayerView
+) : FullscreenHandler {
+
+  override fun onFullscreenRequested() {
+    val context = playerView.context;
+    val intent = Intent(context, FullScreenActivity::class.java)
+    intent.putExtra("sourceItem", playerView.player?.config?.sourceItem)
+    context.startActivity(intent);
+  }
+
+  override fun onFullscreenExitRequested() {
+  }
+
+  override fun onResume() {
+  }
+
+  override fun onPause() {}
+
+  override fun onDestroy() {
+  }
+
+  override fun isFullScreen(): Boolean {
+    return false
+  }
+
+}
--- android/src/main/java/com/takeoffmediareactnativebitmovinplayer/ReactNativeBitmovinPlayer.kt
+++ android/src/main/java/com/takeoffmediareactnativebitmovinplayer/ReactNativeBitmovinPlayer.kt
@@ -1,37 +1,41 @@
 package com.takeoffmediareactnativebitmovinplayer
 
-import android.util.Log
-import com.bitmovin.player.BitmovinPlayer
 import com.bitmovin.player.BitmovinPlayerView
+import com.bitmovin.player.cast.BitmovinCastManager
 import com.bitmovin.player.config.PlayerConfiguration
 import com.bitmovin.player.config.media.SourceItem
-import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
-import com.facebook.react.views.image.ReactImageView
 
 
 class ReactNativeBitmovinPlayer : SimpleViewManager<BitmovinPlayerView>() {
 
-  private lateinit var globalContext: ThemedReactContext
-  private lateinit var playerView: BitmovinPlayerView
-  private lateinit var player: BitmovinPlayer
+  private lateinit var _context: ThemedReactContext
+  private lateinit var _playerView: BitmovinPlayerView
 
   override fun createViewInstance(reactContext: ThemedReactContext): BitmovinPlayerView {
-    Log.i("context: ", reactContext.toString())
-    globalContext = reactContext
-    playerView = BitmovinPlayerView(globalContext)
-    player = playerView.player;
+    _context = reactContext
 
-    return playerView;
+    val playerConfiguration = PlayerConfiguration()
+    playerConfiguration.playbackConfiguration?.autoplayEnabled = false
+    _playerView = BitmovinPlayerView(_context, playerConfiguration)
+
+
+    // Instantiate a custom FullscreenHandler
+    val customFullscreenHandler = CustomFullscreenHandler(_playerView, null)
+    // Set the FullscreenHandler to the BitmovinPlayerView
+    _playerView.setFullscreenHandler(customFullscreenHandler)
+
+    BitmovinCastManager.getInstance().updateContext(reactContext)
+
+    return _playerView;
   }
 
   override fun onDropViewInstance(view: BitmovinPlayerView) {
-    playerView.onDestroy()
+    _playerView.onDestroy()
     super.onDropViewInstance(view)
-
   }
 
   override fun getName(): String {
@@ -40,8 +44,6 @@ class ReactNativeBitmovinPlayer : SimpleViewManager<BitmovinPlayerView>() {
 
   @ReactProp(name = "configuration")
   fun setConfiguration(playerView: BitmovinPlayerView, configProps: ReadableMap?) {
-    val config = PlayerConfiguration()
-    config.sourceItem = SourceItem(configProps?.getString("url"))
-    playerView.player.setup(config)
+    playerView.player?.load(SourceItem(configProps?.getString("url")))
   }
 }
