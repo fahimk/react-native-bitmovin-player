f373f6079e1102f16e6e7ebc04c5c0c27568c062
Fahim Karim <fahimkarim@gmail.com>
2021-02-28 12:11:25 -0800

    listen to lifecycle changes

--- android/src/main/java/com/takeoffmediareactnativebitmovinplayer/ReactNativeBitmovinPlayer.kt
+++ android/src/main/java/com/takeoffmediareactnativebitmovinplayer/ReactNativeBitmovinPlayer.kt
@@ -4,42 +4,54 @@ import com.bitmovin.player.BitmovinPlayerView
 import com.bitmovin.player.cast.BitmovinCastManager
 import com.bitmovin.player.config.PlayerConfiguration
 import com.bitmovin.player.config.media.SourceItem
+import com.facebook.react.bridge.LifecycleEventListener
 import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
 
 
-class ReactNativeBitmovinPlayer : SimpleViewManager<BitmovinPlayerView>() {
-
-  private lateinit var _context: ThemedReactContext
-  private lateinit var _playerView: BitmovinPlayerView
+class ReactNativeBitmovinPlayer : SimpleViewManager<BitmovinPlayerView>(), LifecycleEventListener {
+  private lateinit var context: ThemedReactContext
+  private lateinit var playerView: BitmovinPlayerView
 
   override fun createViewInstance(reactContext: ThemedReactContext): BitmovinPlayerView {
-    _context = reactContext
+    context = reactContext
 
     val playerConfiguration = PlayerConfiguration()
     playerConfiguration.playbackConfiguration?.autoplayEnabled = false
-    _playerView = BitmovinPlayerView(_context, playerConfiguration)
+    this.playerView = BitmovinPlayerView(this.context, playerConfiguration)
 
 
     // Instantiate a custom FullscreenHandler
-    val customFullscreenHandler = CustomFullscreenHandler(_playerView, null)
+    val customFullscreenHandler = CustomFullscreenHandler(this.playerView)
     // Set the FullscreenHandler to the BitmovinPlayerView
-    _playerView.setFullscreenHandler(customFullscreenHandler)
+    this.playerView.setFullscreenHandler(customFullscreenHandler)
 
     BitmovinCastManager.getInstance().updateContext(reactContext)
 
-    return _playerView;
+    return this.playerView;
   }
 
   override fun onDropViewInstance(view: BitmovinPlayerView) {
-    _playerView.onDestroy()
+    this.playerView.onDestroy()
     super.onDropViewInstance(view)
   }
 
   override fun getName(): String {
     return "ReactNativeBitmovinPlayer"
+  }
+
+  override fun onHostResume() {
+    this.playerView.onResume()
+  }
+
+  override fun onHostPause() {
+    this.playerView.onPause()
+  }
+
+  override fun onHostDestroy() {
+    this.playerView.onDestroy()
   }
 
   @ReactProp(name = "configuration")
